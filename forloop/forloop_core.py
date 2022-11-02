a6cc12e25cf38a0c2e36e4dd24d2f8501c7ae824
Dominik Vach <vach.dominik@gmail.com>
2022-11-02 21:31:45 +0100

    update API client

--- forloop/forloop_core.py
+++ forloop/forloop_core.py
@@ -1,6 +1,7 @@
 
 import requests
 import json
+import pandas as pd
 
 class ForloopClient:
     def __init__(self, key=None, secret=None, url=None):
@@ -37,9 +38,7 @@ class ForloopClient:
     def clean_data(self,filename):
         payload={"filename":filename}
         response=requests.post(self.url+"/api/v1/clean_data",data=json.dumps(payload))
-        
-        
-        result=pd.read_pickle("C:\\Users\\EUROCOM\\Documents\\Git\\ForloopAI\\forloop_platform_dominik\\cleaned_data.pkl")
+        result=response.json()
         return(result)
     
     
@@ -57,34 +56,4 @@ class ForloopClient:
         result=response.json()#["results"]
         return(result)
 
-
     
-    
-
-
-
-################# OLDER ##################
-import pandas as pd
-from idfops.pandas_operations import read_spreadsheet
-
-#Package functions
-def load_df(filename):
-    try:
-        df=read_spreadsheet(filename)[0]
-    except FileNotFoundError as e:
-        print("File was not found")
-        df=None
-    return(df)
-
-
-def send_data_to_forloop(df):
-    """TODO: Implementation"""
-    pass
-
-
-def import_batch_data():
-    """
-    Loading data from batches coming from the platform
-    TODO: Implementation
-    """
-    pass
