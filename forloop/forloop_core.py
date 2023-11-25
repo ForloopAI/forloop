45c16e9ff8699d9798546ff47699b9e1d87e8c4b
Daniel Chmúrny <daniel123chmurny@gmail.com>
2023-11-25 22:13:19 +0100

    Clear code and disable methods leading to non-existent endpoints

--- forloop/forloop_core.py
+++ forloop/forloop_core.py
@@ -1,13 +1,11 @@
 
 import requests
 import json
-import pandas as pd
 
 class ForloopClient:
     def __init__(self, key=None, secret=None, url=None):
         self.key = key
         self.secret = secret
-        #self.session = requests.Session()
 
         if url:
             self.url = url
@@ -15,45 +13,33 @@ class ForloopClient:
             self.url = "https://www.forloop.ai"
 
     
-    def get_nodes(self,pipeline):
+    def __get_nodes(self,pipeline):
         response=requests.get(self.url+"/api/v1/nodes")
         print(response,response.content)
         
         result=response.json()["results"]
         return(result)
 
-
-    def analyze_data(self,filename):
+    def __analyze_data(self,filename):
         payload={"filename":filename}
         response=requests.post(self.url+"/api/v1/analyze_data",data=json.dumps(payload))
+        result=response.json()
         
-        
-        #print("RESPONSE",response,response.content)
-        
-        
-        result=response.json()#["results"]
-        return(result)
+        return result
 
-
-    def clean_data(self,filename):
+    def __clean_data(self,filename):
         payload={"filename":filename}
         response=requests.post(self.url+"/api/v1/clean_data",data=json.dumps(payload))
         result=response.json()
-        return(result)
-    
-    
-    def run_python_script(self,filename,dir_path):
-        #dir_path="C:\\Users\\EUROCOM\\Documents\\Git\\ForloopAI\\forloop_api"
         
+        return result
+    
+    def __run_python_script(self,filename,dir_path):        
         payload={"filename":filename,"dir_path":dir_path}
         
         response=requests.post(self.url+"/api/v1/run_python_script",data=json.dumps(payload))
+        result=response.json()
         
-        
-        print("RESPONSE",response,response.content)
-        
-        
-        result=response.json()#["results"]
-        return(result)
+        return result
 
     
